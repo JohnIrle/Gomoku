9953c4b0e938b2925f64d7f15f26e443dd95ec7a
JohnIrle <irlejohn@gmail.com>
2019-03-15 17:36:35 -0500

    Initial Commit.

--- src/gomoku/Main.java
+++ src/gomoku/Main.java
@@ -3,5 +3,16 @@ package gomoku;
 // John Irle
 // 07 March 2019
 
-public class Main {
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+public class Main extends Application {
+
+  public void start(Stage primaryStage) {
+
+  }
+
+  public static void main(String[] args) {
+    launch();
+  }
 }
